ccf220171015e69a3f5fe5d30161310262e938b1
Giovanni <giovanni@192.168.0.103>
2024-09-30 17:49:08 -0500

    Day format

--- src/lib/date.lib.ts
+++ src/lib/date.lib.ts
@@ -19,9 +19,10 @@ const getDateFormated = (date:string) => {
   const newDate = new Date(`${date} 00:00`);
   const year = newDate.getFullYear();
   const month = newDate.getMonth();
-  const day = newDate.getDate();
+  const day = `${newDate.getDate()}`;
+  const formattedDay = day.length > 1 ? day : `0${day}`
 
-  return `${MONTHS[month]} ${day}, ${year}`;
+  return `${MONTHS[month]} ${formattedDay}, ${year}`;
 };
 
 const orderNewestFirst = (
